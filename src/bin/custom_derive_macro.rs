7f0bdc54b5f3707c3224e99012f8abbee93bc811
Kshitij886 <kshitijkhatri437@gmail.com>
2026-02-16 14:57:37 +0545

    remaning deserialize

--- src/bin/custom_derive_macro.rs
+++ src/bin/custom_derive_macro.rs
@@ -15,7 +15,16 @@ struct Swap {
     qty_1: u32,
     qty_2: u32,
 }
-
+impl Deserialize for Swap {
+    fn deserialize(base: Vec<u8>) -> Result<Swap, Error> {
+        if base.len() < 8 {
+            return Err(Error);
+        }
+        let qty_1 = base[0..3];
+        let qty_2 = base[4..7];
+        return Ok(Swap { qty_1, qty_2 });
+    }
+}
 impl Serialize for Swap {
     fn serialize(&self) -> Vec<u8> {
         let mut v = Vec::new();
